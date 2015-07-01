0430aad30fbe9de36728fe4e3861276ea5cbef33
Jefferson <jcsantosbr@gmail.com>
2015-07-01 01:29:21 -0300

    Changing clojure version to 1.7

--- template/project.clj
+++ template/project.clj
@@ -1,5 +1,5 @@
 (defproject clojureapp "0.1.0-SNAPSHOT"
     :plugins [[lein-ring "0.7.5"]]
-    :dependencies [[org.clojure/clojure "1.5.1"]
-                   [http-kit "2.1.12"]]
+    :dependencies [[org.clojure/clojure "1.7.0"]
+                   ]
     :main webapp)
