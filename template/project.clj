712e75c1cbd1a1dda70f85ec1f479a4677d7b380
Jefferson <jcsantosbr@gmail.com>
2015-07-02 00:42:37 -0300

    Adding figwheel plugin to cartridge

--- template/project.clj
+++ template/project.clj
@@ -2,7 +2,8 @@
     :plugins [[lein-ring "0.9.1"]
 			  [lein-environ "1.0.0"]
 			  [lein-ancient "0.6.5"]
-			  [lein-cljsbuild "1.0.6"]]
+			  [lein-cljsbuild "1.0.6"]
+			  [lein-figwheel "0.3.3"]]
     :dependencies [[org.clojure/clojure "1.7.0"]]
     :main webapp)
 
